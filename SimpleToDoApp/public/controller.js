6a617d678c8e26932a661de3b534c5067f91a200
Ventsislav <go6o123890@abv.bg>
2015-04-23 03:20:55 +0300

    create and delete todos works

--- SimpleToDoApp/public/controller.js
+++ SimpleToDoApp/public/controller.js
@@ -1,9 +1,34 @@
 var ToDoApp = angular.module('ToDoApp', []);
 
-ToDoApp.controller("MainController",['$scope', function($scope) {
-  $scope.hi = "hello";
-  $scope.createTodo = function(toDoText)
-  {
-	// send todo to server and save it to database
-  }
+ToDoApp.controller("MainController",['$scope','$http', function($scope, $http) {
+  $scope.toDoObj = {};
+  
+	$http.get('/ToDos')
+		.success(function(data) {
+			$scope.todos = data;
+		})
+		.error(function(data) {
+			console.log('Error: ' + data);
+		});
+		
+	$scope.createTodo = function(toDoObj)
+	{
+		$http.post('/CreateToDo',toDoObj)
+		.success(function(data) {
+			$scope.todos = data;
+		})
+		.error(function(data) {
+			console.log('Error: ' + data);
+		});
+	}
+	
+	$scope.deleteTodo = function(toDoId){
+		$http.delete('/todos/' + toDoId)
+			.success(function(data) {
+				$scope.todos = data;
+			})
+			.error(function(data) {
+				console.log('Error: ' + data);
+			});
+	}
 }]);
--- SimpleToDoApp/server.js
+++ SimpleToDoApp/server.js
@@ -1,21 +1,66 @@
 var express = require("express");
 var app = express();
+var mongoose = require("mongoose");
+var bodyParser = require('body-parser')
 var port = 3030;
 
-app.get("/", function(req,res){
-	res.send("HI man")
-})
+mongoose.connect('mongodb://localhost:27017/todos');
 
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser())
+
+
+	
+var ToDo = mongoose.model('ToDo', {
+		text: String 
+	}
+);
+
+// creating todo
 app.post("/CreateToDo",function(req,res){
-	// save todo into database
+	var toDo = new ToDo({ text: req.body.text });
+	
+	toDo.save(function (err) {
+		if (err){
+			console.log("error:" + err)
+		}
+		ToDo.find(function (err, toDos) {
+			if (err) {
+				console.log(err)
+			}
+			res.send(toDos)
+		})
+	});
+		
 })
 
+// get todos
 app.get("/ToDos",function(req,res){
-	// send todos from database to client
+	ToDo.find(function (err, toDos) {
+		if (err) {
+			console.log(err)
+		}
+		
+		res.send(toDos)
+	})
 })
+
+app.delete('/todos/:id', function(req, res) {
+	ToDo.remove({ _id: req.params.id }, function (err) {
+		if (err) {
+			console.log(err)
+		};
+		ToDo.find(function (err, toDos) {
+			if (err) {
+				console.log(err)
+			}
+			res.send(toDos)
+		})		
+	})
+});
 
 app.get("*",function(req,res){
-	res.sendfile("public/index.html");
+	res.sendfile('./public/index.html');
 })
 
 app.listen(port);
